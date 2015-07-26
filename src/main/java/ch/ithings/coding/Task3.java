662e8466285851d97503e6065e2bc39ee8982770
Thomas Pham <thomas.pham@ithings.ch>
2015-07-27 00:58:51 +0200

    completed task 3

--- src/main/java/ch/ithings/coding/Task3.java
+++ src/main/java/ch/ithings/coding/Task3.java
@@ -6,7 +6,33 @@ package ch.ithings.coding;
  */
 public class Task3 {
     
+    public static final int[] A = {1, 8, -3, 0, 1, 3, -2, 4, 5};
+    
     public static void main(String args[]) {
-        System.out.println("Task3 Ends");
+        Task3 task3 = new Task3();
+        task3.solution(6, A);
     }
+    
+    public int solution(int K, int[] A) {
+        
+        if (A.length == 0)
+            return -1;
+        
+        int complPairs = 0;
+        
+        for (int i=0;i<A.length;i++){
+            for (int j=0;j<A.length;j++){
+                //System.out.print("("+i+","+j+")=");
+                //System.out.print(A[i]+"+"+A[j]);
+                //System.out.println("="+(A[i]+A[j]));
+                if (A[i] + A[j] == K){
+                    complPairs = complPairs+1;
+                }
+            }
+        }
+        
+        System.out.println("Complementary Pairs: " + complPairs);
+        return complPairs;
+    }
+    
 }
